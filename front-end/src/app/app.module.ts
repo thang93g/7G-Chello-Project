e8938f970968709a2cf92033dc103a714ad6c91d
ducviethaboo <ducviet300397@gmail.com>
2021-01-06 16:35:24 +0700

    done login lay token

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,12 +29,14 @@ import { ProfileComponent } from './core/profile/profile/profile.component';
   ],
 
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
     MatBadgeModule,
     MatToolbarModule,
+    HttpClientModule,
 
   ],
   providers: [],
