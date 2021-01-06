910b68c1cc43dd2496ba2a81632805d8f5bb313c
ducviethaboo <ducviet300397@gmail.com>
2021-01-06 09:14:47 +0700

    ...

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -5,28 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
-import {MatBadgeModule} from '@angular/material/badge';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BoardpageComponent } from './core/boardpage/boardpage.component';
-
 
 import { NavbarComponent } from './core/homepage/navbar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    BoardpageComponent,
-    NavbarComponent
-  ],
+  declarations: [AppComponent, BoardpageComponent, NavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
     MatBadgeModule,
-    MatToolbarModule
+    MatToolbarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
