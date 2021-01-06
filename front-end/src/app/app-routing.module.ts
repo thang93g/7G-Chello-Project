4f2d692083a45b7347a64a252ba72dad5957c07f
ducviethaboo <ducviet300397@gmail.com>
2021-01-06 21:49:19 +0700

    routing dang nhap dang ky va home page

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -5,9 +5,11 @@ import { SingupComponent } from './singup/singup.component';
 
 
 import { LoginComponent } from './login/login.component'
+import { NavbarComponent } from './core/homepage/navbar.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '', pathMatch: 'full' },
+  // { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '', component: NavbarComponent },
   { path: 'login', component: LoginComponent },
   { path: 'singup', component: SingupComponent },
 
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
 
 
 
