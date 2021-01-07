464b93cadee910bb775d16cff8497983823fb18b
thang93g <manhthangxthqnd@gmail.com>
2021-01-07 08:37:10 +0700

    validate

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-// import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -46,7 +47,6 @@ import { AngularFireStorageModule } from "@angular/fire/storage";
     AngularFireStorageModule,
     AngularFireModule.initializeApp(environment.firebaseConfig, "cloud"),
     HttpClientModule,
-    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
