bd34241a68561fb2df9696fb213e71495c1942ab
thang93g <manhthangxthqnd@gmail.com>
2021-01-11 21:37:24 +0700

    upload front-end

--- back-end/app/Http/Controllers/TaskController.php
+++ back-end/app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Task;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class TaskController extends Controller
@@ -20,19 +21,20 @@ class TaskController extends Controller
             'title' => 'required|string|max:255',
             'label' => 'required|string|max:255',
             'column_id' => 'required',
-            'orders' => 'required|numeric|min:1'
         ]);
 
         if ($validator->fails()) {
             return response()->json($validator->errors()->toJson(), 400);
         }
 
-        $task = Task::create([
-            'title' => $request->get('title'),
-            'label' => $request->get('label'),
-            'column_id' => $request->get('column_id'),
-            'orders' => $request->get('orders'),
-        ]);
+        // $task = Task::create([
+        //     'title' => $request->get('title'),
+        //     'label' => $request->get('label'),
+        //     'column_id' => $request->get('column_id'),
+        //     'orders' => $request->get('orders'),
+        // ]);
+
+        $task = DB::select("CALL autoIncTask('$request->title',$request->label,$request->column_id)");
 
         return response()->json(compact('task'),201);
 
@@ -60,5 +62,19 @@ class TaskController extends Controller
     {
         $task = Task::findOrFail($id);
         $task->delete();
+    }
+
+    public function swap($id,$orders){
+        $task = Task::findOrFail($id);
+        $task->orders = $orders;
+        $task->save();
+        return response()->json($task);
+    }
+
+    public function drop($id,$column_id){
+        $task = Task::findOrFail($id);
+        $task->column_id = $column_id;
+        $task->save();
+        return response()->json($task);
     }
 }
