5825d6e68fc0a613955d7b9139b828a3b97d6036
dvhungg123@gmail.com <dvhungg123@gmail.com>
2021-01-13 11:09:52 +0700

    push lai backend

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -25,46 +25,47 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/users', function (Request $request) {
     return $request->user();
 });
+    Route::get('users', [UserController::class, 'index']);
+    Route::post('login', [UserController::class, 'authenticate']);
+    Route::post('register', [UserController::class, 'register']);
+
+    Route::put('/users/{id}', [UserController::class, 'update']);
+    Route::get('/users/{id}', [UserController::class, 'show']);
 
 
-Route::get('columns/{board_id}',[ColumnController::class,'index']);
-Route::get('columns/show/{id}',[ColumnController::class,'show']);
-Route::post('columns/create',[ColumnController::class,'create']);
-Route::put('columns/update/{id}',[ColumnController::class,'update']);
-Route::get('columns/swap/{id}/{index}',[ColumnController::class,'swap']);
-Route::post('columns/comment', [CommentController::class,'commentOnTask']);
-Route::get('columns/{id}/upload',[FileController::Class,'index']);
-Route::post('columns/{id}/upload',[FileController::Class,'uploadFile']);
+Route::middleware('jwt.verify')->group(function () {
+    Route::get('columns/{board_id}', [ColumnController::class, 'index']);
+    Route::get('columns/show/{id}', [ColumnController::class, 'show']);
+    Route::post('columns/create', [ColumnController::class, 'create']);
+    Route::put('columns/update/{id}', [ColumnController::class, 'update']);
+    Route::get('columns/swap/{id}/{index}', [ColumnController::class, 'swap']);
+    Route::post('columns/comment', [CommentController::class, 'commentOnTask']);
+    Route::get('columns/{id}/upload', [FileController::class, 'index']);
+    Route::post('columns/{id}/upload', [FileController::class, 'uploadFile']);
 
 
-Route::get('tasks',[TaskController::class,'index']);
-Route::post('tasks/create',[TaskController::class,'create']);
-Route::put('tasks/update/{id}',[TaskController::class,'update']);
-Route::delete('tasks/{id}',[TaskController::class,'delete']);
-Route::get('tasks/swap/{id}/{orders}',[TaskController::class,'swap']);
-Route::get('tasks/drop/{id}/{column_id}',[TaskController::class,'drop']);
+    Route::get('tasks', [TaskController::class, 'index']);
+    Route::post('tasks/create', [TaskController::class, 'create']);
+    Route::put('tasks/update/{id}', [TaskController::class, 'update']);
+    Route::delete('tasks/{id}', [TaskController::class, 'delete']);
+    Route::get('tasks/swap/{id}/{orders}', [TaskController::class, 'swap']);
+    Route::get('tasks/drop/{id}/{column_id}', [TaskController::class, 'drop']);
 
-Route::get('boards',[BoardController::class,'index']);
-Route::delete('boards/{id}',[BoardController::class,'destroy']);
-Route::post('boards/{id}',[BoardController::class,'create']);
-Route::get('boards/detail/{id}',[BoardController::class,'getBoardById']);
+    Route::get('boards', [BoardController::class, 'index']);
+    Route::delete('boards/{id}', [BoardController::class, 'destroy']);
+    Route::post('boards/{id}', [BoardController::class, 'create']);
+    Route::get('boards/detail/{id}', [BoardController::class, 'getBoardById']);
 
-Route::get('users',[UserController::class,'index']);
-Route::post('login',[UserController::class,'authenticate']);
-Route::post('register',[UserController::class,'register']);
+    Route::put('changePassword/{id}', [ChangePassword::class, 'changePassword']);
 
+  
+    Route::delete('/groups/member/{id}/{user_id}', [GroupController::class, 'deleteUser']);
+    Route::post('/groups/{id}', [GroupController::class, 'addUser']);
+    Route::put('/groups/{id}', [GroupController::class, 'update']);
+    Route::delete('/groups/{id}', [GroupController::class, 'destroy']);
+    Route::post('/groups', [GroupController::class, 'store']);
+});
 
-Route::put('/users/{id}',[UserController::class,'update']);
-Route::get('/users/{id}',[UserController::class,'show']);
-Route::put('changePassword/{id}', [ChangePassword::class,'changePassword']);
-
-Route::get('/groups/{id}',[GroupController::class,'index']);
-Route::get('/groups/detail/{id}',[GroupController::class,'show']);
-Route::get('/groups/member/{id}',[GroupController::class,'getUser']);
-Route::delete('/groups/member/{id}/{user_id}',[GroupController::class,'deleteUser']);
-Route::post('/groups/{id}',[GroupController::class,'addUser']);
-Route::put('/groups/{id}',[GroupController::class,'update']);
-Route::delete('/groups/{id}',[GroupController::class,'destroy']);
-Route::post('/groups',[GroupController::class,'store']);
-
-
+Route::get('/groups/{id}', [GroupController::class, 'index']);
+Route::get('/groups/detail/{id}', [GroupController::class, 'show']);
+Route::get('/groups/member/{id}', [GroupController::class, 'getUser']);
